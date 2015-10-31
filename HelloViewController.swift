da9afa10140d6fdc8331059d8b7a0fc2aed76c22
Syahmi Ismail <msi@syamey.com>
2015-10-31 12:09:53 +0800

    Add UILabel and UIButton

--- HelloViewController.swift
+++ HelloViewController.swift
@@ -15,11 +15,26 @@ class HelloViewController: UIViewController {
         
         self.view.backgroundColor = UIColor.lightGrayColor()
         
-        let helloworld: UILabel = UILabel()
+        let helloworld: UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: 320, height: 100))
         helloworld.text = "Hello World"
-        helloworld.backgroundColor = UIColor.blackColor()
+        helloworld.textColor = UIColor.blackColor()
+        helloworld.textAlignment = NSTextAlignment.Center
         
         self.view.addSubview(helloworld)
+        
+        let hiButton: UIButton = UIButton(frame: CGRect(x: 20, y: 120, width: 130, height: 40))
+        hiButton.setTitle("Hello there", forState: UIControlState.Normal)
+        hiButton.setTitleColor(UIColor.greenColor(), forState: UIControlState.Normal)
+        hiButton.backgroundColor = UIColor.whiteColor()
+        
+        self.view.addSubview(hiButton)
+        
+        let byeButton: UIButton = UIButton(frame: CGRect(x: 170, y: 120, width: 130, height: 40))
+        byeButton.setTitle("Goodbye", forState: UIControlState.Normal)
+        byeButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+        byeButton.backgroundColor = UIColor.whiteColor()
+        
+        self.view.addSubview(byeButton)
 
         // Do any additional setup after loading the view.
     }
