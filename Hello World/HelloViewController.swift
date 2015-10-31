6450613a6a83795b68091d70a990a0c2391f019e
Syahmi Ismail <msi@syamey.com>
2015-10-31 16:07:45 +0800

    Convert everything to storyboard

--- Hello World/HelloViewController.swift
+++ Hello World/HelloViewController.swift
@@ -9,72 +9,84 @@
 import UIKit
 
 class HelloViewController: UIViewController {
-    var helloworld: UILabel!
-    var hiButton: UIButton!
-    var byeButton: UIButton!
-    var resetButton: UIButton!
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    @IBOutlet weak var hiButton: UIButton!
+    @IBOutlet weak var byeButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor.lightGrayColor()
+        hiButton.layer.cornerRadius = 5.0
+        byeButton.layer.cornerRadius = 5.0
+        resetButton.layer.cornerRadius = 5.0
         
-        helloworld = UILabel(frame: CGRect(x: 0, y: 20, width: 320, height: 100))
-        helloworld.text = "Hello World"
-        helloworld.textColor = UIColor.blackColor()
-        helloworld.textAlignment = NSTextAlignment.Center
-        helloworld.font = UIFont.systemFontOfSize(24)
+//        self.view.backgroundColor = UIColor.lightGrayColor()
+//        
+//        helloWorldLabel = UILabel(frame: CGRect(x: 0, y: 20, width: 320, height: 100))
+//        helloWorldLabel.text = "Hello World"
+//        helloWorldLabel.textColor = UIColor.blackColor()
+//        helloWorldLabel.textAlignment = NSTextAlignment.Center
+//        helloWorldLabel.font = UIFont.systemFontOfSize(24)
+//        
+//        self.view.addSubview(helloWorldLabel)
         
-        self.view.addSubview(helloworld)
-        
-        hiButton = UIButton(frame: CGRect(x: 20, y: 120, width: 130, height: 40))
-        hiButton.setTitle("Hello There", forState: UIControlState.Normal)
-        hiButton.setTitleColor(UIColor.greenColor(), forState: UIControlState.Normal)
+//        hiButton = UIButton(frame: CGRect(x: 20, y: 120, width: 130, height: 40))
+//        hiButton.setTitle("Hello There", forState: UIControlState.Normal)
+//        hiButton.setTitleColor(UIColor.greenColor(), forState: UIControlState.Normal)
         hiButton.addTarget(self, action: "sayHi", forControlEvents: UIControlEvents.TouchUpInside)
-        hiButton.backgroundColor = UIColor.whiteColor()
+//        hiButton.backgroundColor = UIColor.whiteColor()
+//        
+//        self.view.addSubview(hiButton)
         
-        self.view.addSubview(hiButton)
-        
-        byeButton = UIButton(frame: CGRect(x: 170, y: 120, width: 130, height: 40))
-        byeButton.setTitle("Goodbye", forState: UIControlState.Normal)
-        byeButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+//        byeButton = UIButton(frame: CGRect(x: 170, y: 120, width: 130, height: 40))
+//        byeButton.setTitle("Goodbye", forState: UIControlState.Normal)
+//        byeButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
         byeButton.addTarget(self, action: "sayGoodbye", forControlEvents: UIControlEvents.TouchUpInside)
-        byeButton.backgroundColor = UIColor.whiteColor()
-        
-        self.view.addSubview(byeButton)
+//        byeButton.backgroundColor = UIColor.whiteColor()
+//        
+//        self.view.addSubview(byeButton)
 
-        resetButton = UIButton(frame: CGRect(x: 20, y: 180, width: 280, height: 40))
-        resetButton.setTitle("Reset", forState: UIControlState.Normal)
+//        resetButton = UIButton(frame: CGRect(x: 20, y: 180, width: 280, height: 40))
+//        resetButton.setTitle("Reset", forState: UIControlState.Normal)
         resetButton.addTarget(self, action: "reset", forControlEvents: UIControlEvents.TouchUpInside)
-        resetButton.backgroundColor = UIColor.blackColor()
-        
-        self.view.addSubview(resetButton)
+//        resetButton.backgroundColor = UIColor.blackColor()
+//        
+//        self.view.addSubview(resetButton)
         
         // Do any additional setup after loading the view.
     }
     
     func sayHi() {
         print("Hello there")
-        helloworld.text = "Hello There"
-        helloworld.textColor = UIColor.greenColor()
+        helloWorldLabel.text = "Hello There"
+        helloWorldLabel.textColor = UIColor.greenColor()
     }
 
     func sayGoodbye() {
         print("Goodbye")
-        helloworld.text = "Goodbye"
-        helloworld.textColor = UIColor.redColor()
+        helloWorldLabel.text = "Goodbye"
+        helloWorldLabel.textColor = UIColor.redColor()
     }
     
     func reset() {
-        helloworld.text = "Hello World"
-        helloworld.textColor = UIColor.blackColor()
+        helloWorldLabel.text = "Hello World"
+        helloWorldLabel.textColor = UIColor.whiteColor()
+    }
+    
+    @IBAction func sliderControl(sender: AnyObject) {
+        print("Slider")
+        helloWorldLabel.text = "This is Slider"
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
